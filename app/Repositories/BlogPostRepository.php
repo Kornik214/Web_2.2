9c720cffdf2d9ad33e904f2b4326c587cc0e1a1d
Kornik214 <nazar.korniichuk@oa.edu.ua>
2026-06-10 16:54:09 +0300

    Lab_8 Complete blog post repository

--- app/Repositories/BlogPostRepository.php
+++ app/Repositories/BlogPostRepository.php
@@ -1,1 +1,42 @@
 <?php
+
+namespace App\Repositories;
+
+use App\Models\BlogPost as Model;
+
+class BlogPostRepository extends CoreRepository
+{
+    protected function getModelClass()
+    {
+        return Model::class;
+    }
+
+    /**
+     * Отримати список статей
+     */
+    public function getAllWithPaginate()
+    {
+        $columns = [
+            'id',
+            'title',
+            'slug',
+            'is_published',
+            'published_at',
+            'user_id',
+            'category_id',
+        ];
+
+        return $this->startConditions()
+            ->select($columns)
+            ->orderBy('id', 'DESC')
+            ->paginate(25);
+    }
+
+    /**
+     * Отримати модель для редагування
+     */
+    public function getEdit($id)
+    {
+        return $this->startConditions()->find($id);
+    }
+}
